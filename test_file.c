f9ac53c306503b2903f8b5dfdcdeae1360707db1
arjun23496 <arjun.k018@gmail.com>
2018-03-15 17:03:53 -0400

    Simple multithreading architecture

--- test_file.c
+++ test_file.c
@@ -3,6 +3,8 @@
 #include <map>
 #include <string>
 #include <random>
+#include <thread>
+#include <atomic>
 
 #include "TChain.h"
 #include "TFile.h"
@@ -31,65 +33,110 @@ using namespace TMVA;
 // 	}
 // }
 
-int test_file(){
+struct shared_space{
+	std::atomic<bool> gen_output_available;
+	std::atomic<bool> dis_eval_available;
 
-	auto print_network = [](DNN::TDeepNet<DNN::TCpu<Double_t>> &network) {
+	DNN::TCpuMatrix<double> disc_gradients;
+};
 
-		std::vector<DNN::VGeneralLayer<DNN::TCpu<double>> *> layers = network.GetLayers();
+void train_generator(struct shared_space *shared){
+	// Global parameters
+	int batch_size = 10;
+	int steps = 1000;
+	int minibatch = 10;
+	int discriminator_train = 10;
+	int generator_train = 1;
+	int nele = minibatch*1;
+	double learning_rate = 0.001;
+	bool real_train = true;
 
-		for(int i=0; i<layers.size(); i++)
-		{
-			cout << "\n Layer: "<<i+1;
+	// Initialize pointers
+	double *g_input_ptr = NULL;
+	double *g_output_ptr = NULL;
+	double *g_target_ptr = NULL;
+	double *weights_ptr = NULL;
 
-			std::vector<DNN::TCpuMatrix<double>> layer_weights = layers[i]->GetWeights();
+	// Set random number generators
+	std::default_random_engine rand_generator;
+	std::uniform_real_distribution<double> source_distribution(0.0,1.0);
+	std::normal_distribution<double> target_distribution(1.0,1.0);
 
-			DNN::TCpuMatrix<double> weights = layer_weights[0];
+	std::cout << "\ngenerator training started";
 
-			for(int row=0; row<weights.GetNrows(); row++)
-			{
-				cout<<"\n";
-				for(int col=0; col<weights.GetNcols(); col++)
-				{
-					cout<<weights(row, col)<<" ";
-				}
-			}
-		}
-	};
+	// Layer definitions
+	DNN::EInitialization init = DNN::EInitialization::kGauss;
+	DNN::EActivationFunction afunc = DNN::EActivationFunction::kSigmoid;
+	DNN::EActivationFunction fact = DNN::EActivationFunction::kTanh;
+	DNN::ERegularization reg = DNN::ERegularization::kNone;
+	DNN::ELossFunction loss = DNN::ELossFunction::kCrossEntropy;
 
-	auto print_gradients = [](DNN::TDeepNet<DNN::TCpu<Double_t>> &network) {
+	// START generator network
 
-		std::vector<DNN::VGeneralLayer<DNN::TCpu<double>> *> layers = network.GetLayers();
+	// Layer definitions
+	DNN::TDenseLayer<DNN::TCpu<Double_t>> g_input_layer = DNN::TDenseLayer<DNN::TCpu<Double_t>>(batch_size, 1, 10, init, 0, afunc, reg, 0);
+	DNN::TDenseLayer<DNN::TCpu<Double_t>> g_output_layer = DNN::TDenseLayer<DNN::TCpu<Double_t>>(batch_size, 10, 1, init, 0, fact, reg, 0);	
 
-		for(int i=0; i<layers.size(); i++)
-		{
-			cout << "\n Layer: "<<i+1;
+	// Initialize network
+	DNN::TDeepNet<DNN::TCpu<Double_t>> generator;
 
-			std::vector<DNN::TCpuMatrix<double>> layer_gradients = layers[i]->GetWeightGradients();
+	// Add dense layer to network
+	generator.AddDenseLayer(&g_input_layer);
+	generator.AddDenseLayer(&g_output_layer);
+	generator.SetLossFunction(loss);
 
-			DNN::TCpuMatrix<double> gradients = layer_gradients[0];
+	// END generator network
 
-			for(int row=0; row<gradients.GetNrows(); row++)
-			{
-				cout<<"\n";
-				for(int col=0; col<gradients.GetNcols(); col++)
-				{
-					cout<<gradients(row, col)<<" ";
-				}
-			}
-		}
-	};
+	// Initialize networks
+	generator.Initialize();
 
-	auto print_matrix = [](DNN::TCpuMatrix<Double_t> &matrix) {
-		for(int row=0; row<matrix.GetNrows(); row++)
-		{
-			cout<<"\n";
-			for(int col=0; col<matrix.GetNcols(); col++)
-			{
-				cout<<matrix(row, col)<<" ";
-			}
-		}
-	};
+	// Initialize input arrays
+	DNN::TCpuMatrix<double> g_input = DNN::TCpuMatrix<double>(minibatch, 1);
+	DNN::TCpuMatrix<double> g_output = DNN::TCpuMatrix<double>(minibatch, 1);
+	DNN::TCpuMatrix<double> g_target = DNN::TCpuMatrix<double>(minibatch, 1);
+	DNN::TCpuMatrix<double> weights = DNN::TCpuMatrix<double>(minibatch, 1);
 
+	// create input vectors
+	std::vector<DNN::TCpuMatrix<double>> g_input_v = { g_input };
+	std::vector<DNN::TCpuMatrix<double>> g_output_v = { g_output };
+
+	g_input_ptr = g_input.GetRawDataPointer();
+	g_output_ptr = g_output.GetRawDataPointer();
+	g_target_ptr = g_target.GetRawDataPointer();
+	weights_ptr = weights.GetRawDataPointer();
+
+	// Intialize constant vectors
+	for(int i=0; i<nele; i++)
+	{
+		// Initialize constant target pointers
+		weights_ptr[i] = 0.5;
+	}
+
+	// // Training
+	// for(int step=1; step<=steps; step++)
+	// {
+	// 	// cout<<"\n Network at "<<step;
+	// 	// print_network(discriminator);
+	// 	// print_gradients(discriminator);
+
+	// 	if(real_train){
+	// 		// Intialize array with values
+	// 		for(int i=0; i<nele; i++)
+	// 		{
+	// 			d_input_ptr[i] = target_distribution(rand_generator);
+	// 		}
+
+	// 		discriminator.Forward(d_input_v);
+			
+	// 		std::cout << "\n Loss: "<< discriminator.Loss(d_target_real, weights);
+
+	// 		discriminator.Backward(d_input_v, d_target_real, weights);
+	// 		discriminator.Update(learning_rate);
+	// 	}
+	// }
+}
+
+void train_discriminator(struct shared_space *shared){
 	// Global parameters
 	int batch_size = 10;
 	int steps = 1000;
@@ -103,11 +150,8 @@ int test_file(){
 	// Initialize pointers
 	double *d_input_ptr = NULL;
 	double *d_output_ptr = NULL;
-	double *g_input_ptr = NULL;
-	double *g_output_ptr = NULL;
 	double *d_target_real_ptr = NULL;
 	double *d_target_fake_ptr = NULL;
-	double *g_target_ptr = NULL;
 	double *weights_ptr = NULL;
 
 	// Set random number generators
@@ -115,10 +159,7 @@ int test_file(){
 	std::uniform_real_distribution<double> source_distribution(0.0,1.0);
 	std::normal_distribution<double> target_distribution(1.0,1.0);
 
-	std::cout << "Start GAN Training";
-	// Simple GAN for taking values
-	
-	std::cout << "GAN for taking values for generating normal distribution";
+	std::cout << "Discriminator Training started";
 
 	// Layer definitions
 	DNN::EInitialization init = DNN::EInitialization::kGauss;
@@ -142,26 +183,9 @@ int test_file(){
 	discriminator.SetLossFunction(loss);
 
 	// END Discriminator Network
-	
-	// START generator network
-
-	// Layer definitions
-	DNN::TDenseLayer<DNN::TCpu<Double_t>> g_input_layer = DNN::TDenseLayer<DNN::TCpu<Double_t>>(batch_size, 1, 10, init, 0, afunc, reg, 0);
-	DNN::TDenseLayer<DNN::TCpu<Double_t>> g_output_layer = DNN::TDenseLayer<DNN::TCpu<Double_t>>(batch_size, 10, 1, init, 0, fact, reg, 0);	
-
-	// Initialize network
-	DNN::TDeepNet<DNN::TCpu<Double_t>> generator;
-
-	// Add dense layer to network
-	discriminator.AddDenseLayer(&g_input_layer);
-	discriminator.AddDenseLayer(&g_output_layer);
-	discriminator.SetLossFunction(loss);
-
-	// END generator network
 
 	// Initialize networks
 	discriminator.Initialize();
-	generator.Initialize();
 
 	// discriminator.Print();
 
@@ -183,11 +207,8 @@ int test_file(){
 
 	d_input_ptr = d_input.GetRawDataPointer();
 	d_output_ptr = d_output.GetRawDataPointer();
-	g_input_ptr = g_input.GetRawDataPointer();
-	g_output_ptr = g_output.GetRawDataPointer();
 	d_target_real_ptr = d_target_real.GetRawDataPointer();
 	d_target_fake_ptr = d_target_fake.GetRawDataPointer();
-	g_target_ptr = g_target.GetRawDataPointer();
 	weights_ptr = weights.GetRawDataPointer();
 
 	// Intialize constant vectors
@@ -202,10 +223,6 @@ int test_file(){
 	// Training
 	for(int step=1; step<=steps; step++)
 	{
-		// cout<<"\n Network at "<<step;
-		// print_network(discriminator);
-		// print_gradients(discriminator);
-
 		if(real_train){
 			// Intialize array with values
 			for(int i=0; i<nele; i++)
@@ -219,14 +236,24 @@ int test_file(){
 
 			discriminator.Backward(d_input_v, d_target_real, weights);
 			discriminator.Update(learning_rate);
-
 		}
 	}
+}
 
-	// cout<<"\n";
-	// discriminator.Print();
+int test_file(){
+	pthread_t generator_thread, discriminator_thread;
+	struct shared_space *shared = (struct shared_space*)malloc(sizeof(struct shared_space));
+	std::thread disc_thread;
 
-	// print_layer_weights();
+	shared->dis_eval_available = false;
+	shared->gen_output_available = false;
+
+	std::cout << "\nStarting threads";
+
+	disc_thread = std::thread(train_discriminator, shared);
+
+	disc_thread.join();
+	std::cout << "\nDiscriminator thread done";
 
 	return 0;
 }
